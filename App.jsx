59b893141217941a741a2a20ceb35e0cd50bb991
Facundo Ferraris <facundo.ferraris@est.fi.uncoma.edu.ar>
2025-01-12 23:02:24 -0300

    Primer commit

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer as Nav } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <>
+    <StatusBar hidden={true} />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Nav>
+
+      </Nav>
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
