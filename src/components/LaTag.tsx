61b3e2149a0c10bac88b67081787cf1a64111c4d
Facundo Ferraris <facundo.ferraris@est.fi.uncoma.edu.ar>
2025-01-18 16:37:42 -0300

    laTag test 7

---
+++ src/components/LaTag.tsx
@@ -0,0 +1,13 @@
+// Components
+import { View,Text } from "react-native";
+
+// Gallery
+const mockUp = require('../assets/PerkCans/lata.png')
+
+export default function LaTag(){
+    return(
+    <View>
+        <Text>Hola</Text>
+    </View>
+    );
+}
