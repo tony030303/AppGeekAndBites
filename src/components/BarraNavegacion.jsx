a97f279e7210e7f8ccff593a9fc51641282c83ea
Facundo Ferraris <facundo.ferraris@est.fi.uncoma.edu.ar>
2025-01-18 16:08:39 -0300

    test laTag 4

--- src/components/BarraNavegacion.jsx
+++ src/components/BarraNavegacion.jsx
@@ -8,7 +8,7 @@ import Intro from '../pages/Section-intro';
 import Comidas from '../pages/Section-comida';
 import Comics from '../pages/Section-comics';
 import Contactos from '../pages/Section-contactos';
-import Anima from './ScrollAnimation';
+import Anima from './prueba';
 
 const Tab = createMaterialTopTabNavigator();
 
---
+++ src/components/Page.tsx
@@ -0,0 +1,116 @@
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Animated, {
+  useAnimatedStyle,
+  SharedValue,
+  interpolate,
+  Extrapolation,
+} from "react-native-reanimated";
+
+interface PageProps {
+  title: string;
+  index: number;
+  translateX: SharedValue<number>;
+}
+
+const { height, width } = Dimensions.get("window");
+
+const SIZE = width * 0.95;
+
+const Page: React.FC<PageProps> = ({ index, title, translateX }) => {
+  const inputRange = [(index - 1) * width, index * width, (index + 1) * width];
+
+  const rStyle = useAnimatedStyle(() => {
+    const scale = interpolate(
+      translateX.value,
+      inputRange,
+      [0, 1, 0],
+      Extrapolation.CLAMP //En caso de extrapolarse
+    );
+
+    const borderRadius = interpolate(
+      translateX.value,
+      inputRange,
+      [0, SIZE / 2, 0], //Al estar en la pagina sera un circulo, al movernos sera un cuadrado
+      Extrapolation.CLAMP //En caso de extrapolarse
+    );
+
+    return {
+      //borderRadius,
+      transform: [{ scale }],
+    };
+  });
+
+  const rTextStyle = useAnimatedStyle(() => {
+    const translateY = interpolate(
+      translateX.value,
+      inputRange,
+      [height / 2, 0, -height / 2],
+      Extrapolation.CLAMP //En caso de extrapolarse
+    );
+
+    const opacity = interpolate(
+      translateX.value,
+      inputRange,
+      [-1, 1, -1],
+      Extrapolation.CLAMP
+    );
+
+    return {
+      opacity,
+      transform: [
+        {
+          translateY,
+        },
+      ],
+    };
+  });
+
+  return (
+    <View
+      style={[
+        styles.pageContainer,
+        { backgroundColor: `rgba(0,0,256,0.${index + 2})` },
+      ]}
+    >
+      <Animated.View style={[styles.square, rStyle]}>
+        <Animated.Image
+          source={require("../assets/PerkCans/lata.png")}
+          style={[rStyle, styles.image]}
+        />
+      </Animated.View>
+      <Animated.View style={[rTextStyle, { position: "absolute" }]}>
+        <Text style={styles.text}>{title}</Text>
+      </Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    width,
+    height,
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+  square: {
+    //funciona como un contenedor para la imagen
+    height: SIZE,
+    width: SIZE,
+    //backgroundColor: "rgba(0,0,256,0.4)",
+  },
+  text: {
+    fontSize: 60,
+    color: "white",
+    textTransform: "uppercase",
+    fontWeight: "700",
+  },
+  image: {
+    height: SIZE,
+    width: SIZE,
+    resizeMode: "contain",
+  },
+});
+
+export { Page };
