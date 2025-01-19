8f82f42cd496b160f77359d1bc0f7334368f28c7
albanyPetit <albanydaniellepetit@gmail.com>
2025-01-19 17:26:20 -0300

    pruebas seccion comida

---
+++ src/jsons/comidas.js
@@ -0,0 +1,8 @@
+const comidas = [
+  { id: 1, image: require("../assets/comidas/hamburguesa.png") },
+  { id: 2, image: require("../assets/comidas/nachos.png") },
+  { id: 3, image: require("../assets/comidas/papas.png") },
+  { id: 4, image: require("../assets/comidas/pizza.png") },
+];
+
+export default comidas;
--- src/pages/Section-comida.jsx
+++ src/pages/Section-comida.jsx
@@ -1,55 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { Animated, StyleSheet, View, Text } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { ImageBackground } from 'expo-image';
-// Componentes
-import Filimina from '../components/Filimina';
-
-const Fondo = require('../assets/neonRoad.gif');
-const Imagen1 = require('../assets/01-index-kids-eating.jpeg')
-const wallpaper = require('../assets/wallPaper/NeonRoad-02.gif')
-import Data from '../jsons/indexContent.json';
-import Credito from '../jsons/creditos.json';
-
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Menu from "../pages/Section-menu";
+import comidasData from "../jsons/comidas"; //importamos el archivo de comidas
+import { ImageBackground } from "expo-image";
+const LIST_DATA = comidasData;
+const wallpaper = require("../assets/wallPaper/NeonRoad-02.gif");
 
 export default function Comida() {
-    
-
   return (
-<>
-   <ImageBackground
-      source={wallpaper}
-      style={style.background}
-      contentFit="cover"
-    >
-   <ScrollView contentContainerStyle={style.scrollContainer}>
-
-   </ScrollView>
-    </ImageBackground>
-</>
+    <SafeAreaProvider>
+      <ImageBackground
+        source={wallpaper}
+        style={style.background}
+        contentFit="cover"
+      >
+        <Menu />
+      </ImageBackground>
+    </SafeAreaProvider>
   );
 }
 
 const style = StyleSheet.create({
   background: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    padding: 20,
-    alignItems: 'center', // Centra el contenido horizontalmente
-  },
-  sectionContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent', // Asegura que sea transparente
-  },
-  text: {
-    fontSize: 24,
-    color: 'black', // Asegura que el texto sea visible sobre el fondo
-    fontFamily: 'comic',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
