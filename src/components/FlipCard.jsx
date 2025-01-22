125f30247dd7b9a032d1cc32bc79b449e0665e16
Facundo Ferraris <facundo.ferraris@est.fi.uncoma.edu.ar>
2025-01-22 18:37:05 -0300

    agregado componente flipcard

---
+++ src/components/FlipCard.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useRef } from "react";
+import {
+  Animated,
+  View,
+  TouchableWithoutFeedback,
+  StyleSheet,
+} from "react-native";
+
+const FlipCard = ({ frente, atras, ...props }) => {
+  const [volteado, setVolteado] = useState(false);
+  const animacionVoltear = useRef(new Animated.Value(0)).current;
+
+  const frenteInterpolado = animacionVoltear.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["0deg", "180deg"],
+  });
+
+  const VoltearAFrenteEstilizado = {
+    transform: [{ rotateY: frenteInterpolado }],
+  };
+
+  const atrasInterpolado = animacionVoltear.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["180deg", "360deg"],
+  });
+
+  const VoltearAAtrasEstilizado = {
+    transform: [{ rotateY: atrasInterpolado }],
+  };
+
+  const voltearCarta = () => {
+    if (volteado) {
+      // Animación de atrás a adelante
+      Animated.spring(animacionVoltear, {
+        toValue: 0,
+        friction: 8,
+        tension: 10,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      // Animación de adelante a atrás
+      Animated.spring(animacionVoltear, {
+        toValue: 180,
+        friction: 8,
+        tension: 10,
+        useNativeDriver: false,
+      }).start();
+    }
+    setVolteado(!volteado);
+  };
+
+  // Combinar estilos base con estilos personalizados
+  const cartaContenedorEstilo = StyleSheet.flatten([
+    styles.cartaContenedor,
+    props.style, // Estilos personalizados pasados como props
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={voltearCarta}>
+        <View style={cartaContenedorEstilo} {...props}>
+          <Animated.View
+            style={[styles.front, styles.card, VoltearAFrenteEstilizado]}
+          >
+            {frente}
+          </Animated.View>
+          <Animated.View
+            style={[styles.back, styles.card, VoltearAAtrasEstilizado]}
+          >
+            {atras}
+          </Animated.View>
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cartaContenedor: {
+    width: 300, // Tamaño base
+    height: 200, // Tamaño base
+    position: "relative",
+  },
+  front: {
+    backgroundColor: "red",
+  },
+  back: {
+    backgroundColor: "blue",
+  },
+  card: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    position: "absolute",
+    backfaceVisibility: "hidden",
+  },
+});
+
+export default FlipCard;
--- src/components/PerksCard.tsx
+++ src/components/PerksCard.tsx
@@ -25,14 +25,14 @@ const PerksCard: React.FC<PerksProps> = ({ index, ruta, translateX }) => {
       translateX.value,
       inputRange,
       [0, 1, 0],
-      Extrapolation.CLAMP //En caso de extrapolarse
+      Extrapolation.CLAMP, //En caso de extrapolarse
     );
 
     const borderRadius = interpolate(
       translateX.value,
       inputRange,
       [0, SIZE / 2, 0], //Al estar en la pagina sera un circulo, al movernos sera un cuadrado
-      Extrapolation.CLAMP //En caso de extrapolarse
+      Extrapolation.CLAMP, //En caso de extrapolarse
     );
 
     return {
@@ -46,14 +46,14 @@ const PerksCard: React.FC<PerksProps> = ({ index, ruta, translateX }) => {
       translateX.value,
       inputRange,
       [height / 2, 0, -height / 2],
-      Extrapolation.CLAMP //En caso de extrapolarse
+      Extrapolation.CLAMP, //En caso de extrapolarse
     );
 
     const opacity = interpolate(
       translateX.value,
       inputRange,
       [-1, 1, -1],
-      Extrapolation.CLAMP
+      Extrapolation.CLAMP,
     );
 
     return {
