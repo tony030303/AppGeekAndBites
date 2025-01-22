d28ecd762c582f978b1fb9deff1a22709a173154
Facundo Ferraris <facundo.ferraris@est.fi.uncoma.edu.ar>
2025-01-22 10:43:29 -0300

    agregado RotadorPressable funcional

--- src/components/TouchRotador.jsx
+++ src/components/TouchRotador.jsx
@@ -1,47 +1,65 @@
-import React, { useEffect, useRef } from "react";
-import { Animated, Easing, TouchableOpacity } from "react-native";
+import React, { Children, useEffect, useRef, useState } from "react";
+import { Animated, Easing, TouchableOpacity, View } from "react-native";
 
 const logo = require("../assets/01-logo.jpeg");
-
 // TAG
-const RotarView = (props) => {
+const RotarPressable = (props) => {
+  const [animation, setAnimation] = useState(false);
   const rotar = useRef(new Animated.Value(0)).current; // Initial value for rotation: 0
 
+  function toggleAnimation() {
+    setAnimation(!animation);
+  }
+
   useEffect(() => {
-    // Usamos Animated.loop para hacer que la animación sea infinita
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(rotar, {
-          toValue: 1, // Final rotation value (360 degrees)
-          duration: 750, // Duration of one complete rotation
-          easing: Easing.linear,
-          useNativeDriver: true,
-        }),
-        Animated.timing(rotar, {
-          toValue: 0, // Final rotation value (360 degrees)
-          duration: 750, // Duration of one complete rotation
-          easing: Easing.linear,
-          useNativeDriver: true,
-        }),
-      ]),
-    ).start(); // Comienza la animación
-  }, []);
+    if (animation) {
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(rotar, {
+            toValue: 1, // Final rotation value (360 degrees)
+            duration: 750, // Duration of one complete rotation
+            easing: Easing.linear,
+            useNativeDriver: true,
+          }),
+          Animated.timing(rotar, {
+            toValue: 0, // Final rotation value (360 degrees)
+            duration: 750, // Duration of one complete rotation
+            easing: Easing.linear,
+            useNativeDriver: true,
+          }),
+        ]),
+      ).start(); // Comienza la animación
+    } else {
+      rotar.setValue(0);
+    }
+  }, [animation]);
   // Interpolamos el valor de rotación para que sea de 0 a 360 grados
   const rotateInterpolate = rotar.interpolate({
     inputRange: [0, 1],
     outputRange: ["0deg", "90deg"], // Convertimos el valor numérico a grados
   });
+
+  const scaleInterpolate = rotar.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1, 1.2, 1], // Convertimos el valor numérico a grados
+  });
+
   console.log("Realizando rotacion");
   return (
-    <Animated.TouchableOpacity
+    <Animated.View
       style={{
         ...props.style,
-        transform: [{ rotateY: rotateInterpolate }], // Aplicamos la rotación interpolada
+        transform: [
+          { rotateY: rotateInterpolate },
+          { scale: scaleInterpolate },
+        ], // Aplicamos la rotación interpolada
       }}
     >
-      {props.children}
-    </Animated.TouchableOpacity>
+      <TouchableOpacity onPress={toggleAnimation}>
+        {props.children}
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
-export default RotarView;
+export default RotarPressable;
